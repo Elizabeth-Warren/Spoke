d7498d3d3c6c7cad182ba683a538611dd5acada4
Larry Person <lp10011@gmail.com>
2019-06-15 14:10:25 -0400

    checkpoint -- PeopleList basically works

--- src/components/PeopleList/index.jsx
+++ src/components/PeopleList/index.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react'
 import type from 'prop-types'
 import FlatButton from 'material-ui/FlatButton'
@@ -8,11 +7,23 @@ import { withRouter } from 'react-router'
 import gql from 'graphql-tag'
 import LoadingIndicator from '../../components/LoadingIndicator'
 import DataTables from 'material-ui-datatables'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import MenuItem from 'material-ui/MenuItem'
+import UserEditDialog from './UserEditDialog'
+import ResetPasswordDialog from './ResetPasswordDialog'
+import { getHighestRole, ROLE_HIERARCHY } from '../../lib'
+import { dataTest } from '../../lib/attributes'
 
+import PeopleIcon from 'material-ui/svg-icons/social/people'
+import Empty from '../../components/Empty'
+
+const INITIAL_PAGE_SIZE = 10
 
 const prepareDataTableData = (users) => users.map(user => ({
+  texterId: user.id,
   texter: user.displayName,
-  email: user.email
+  email: user.email,
+  roles: user.roles
 })
 )
 
@@ -21,7 +32,19 @@ export class PeopleList extends Component {
     super(props)
 
     this.state = {
+      open: false,
+      userEdit: undefined,
+      pageSize: INITIAL_PAGE_SIZE,
+      cursor: {
+        offset: 0,
+        limit: INITIAL_PAGE_SIZE
+      },
+      passwordResetHash: ''
     }
+
+    this.requestUserEditClose = this.requestUserEditClose.bind(this)
+    this.updateUser = this.updateUser.bind(this)
+    this.handlePasswordResetClose = this.handlePasswordResetClose.bind(this)
   }
 
   componentDidUpdate = (prevProps) => {
@@ -45,26 +68,168 @@ export class PeopleList extends Component {
         overflow: 'scroll',
         whiteSpace: 'pre-line'
       }
+    },
+    {
+      key: 'roles',
+      label: 'Role',
+      style: {
+        textOverflow: 'ellipsis',
+        overflow: 'scroll',
+        whiteSpace: 'pre-line'
+      },
+      render: this.renderRolesDropdown
+    },
+    {
+      key: 'edit',
+      label: '',
+      style: {
+        textOverflow: 'ellipsis',
+        overflow: 'scroll',
+        whiteSpace: 'pre-line'
+      },
+      render: this.renderEditButton
+    },
+    {
+      key: 'password',
+      label: '',
+      style: {
+        textOverflow: 'ellipsis',
+        overflow: 'scroll',
+        whiteSpace: 'pre-line'
+      },
+      render: this.renderChangePasswordButton
     }
   ]
 
-  handleNextPageClick = () => {
+  editUser(userId) {
+    this.setState({ 
+      userEdit: userId
+    })
+  }
+
+  updateUser() {
+    this.setState({ 
+      userEdit: false,
+      forceUpdateTime: Date.now()
+    })
+  }
+
+  async resetPassword(userId) {
+    const { currentUser } = this.props
+    if (currentUser.id !== userId) {
+      const res = await this
+        .props
+        .mutations
+        .resetUserPassword(this.props.organizationId, userId)
+      this.setState({ passwordResetHash: res.data.resetUserPassword })
+    }
+  }
+
+  renderRolesDropdown = (columnKey, row) => {
+    const { roles, texterId } = row
+    const { currentUser } = this.props
+    return (
+      <DropDownMenu
+        value={getHighestRole(roles)}
+        disabled={texterId === currentUser.id || getHighestRole(roles) === 'OWNER' && getHighestRole(currentUser.roles) !== 'OWNER'}
+        onChange={(event, index, value) => this.handleChange(texterId, value)}
+      >
+        {ROLE_HIERARCHY.map((option) => (
+          <MenuItem
+            key={texterId + '_' + option}
+            value={option}
+            disabled={option === 'OWNER' && getHighestRole(currentUser.roles) !== 'OWNER'}
+            primaryText={`${option.charAt(0).toUpperCase()}${option.substring(1).toLowerCase()}`}
+          />
+        ))}
+      </DropDownMenu>
+    )
+  }
+
+  renderEditButton = (columnKey, row) => {
+    const { texterId } = row
+    return (
+      <FlatButton
+        {...dataTest('editPerson')}
+        label='Edit'
+        onTouchTap={() => { this.editUser(texterId) }}
+      />
+    )
+  }
+
+  renderChangePasswordButton = (columnKey, row) => {
+    const { texterId } = row
+    const { currentUser } = this.props
+    return (
+      <FlatButton
+        label='Reset Password'
+        disabled={currentUser.id === texterId}
+        onTouchTap={() => { this.resetPassword(texterId) }}
+      />
+    )
+  }
+
+  changePage = (pageDelta, pageSize) => {
     const { limit, offset, total } = this.props.users.people.pageInfo
     const currentPage = Math.floor(offset / limit)
     const maxPage = Math.floor(total / limit)
-    const newPage = Math.min(maxPage, currentPage + 1)
-    this.props.onPageChanged(newPage)
+    const newPage = Math.min(maxPage, currentPage + pageDelta)
+    this.props.users.fetchMore({
+      variables: {
+        cursor: {
+          offset: newPage * pageSize,
+          limit: pageSize
+        }
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        const returnValue = {
+          people: {
+            users: []
+          }
+        }
+
+        if (fetchMoreResult) {
+          returnValue.people.users = fetchMoreResult.data.people.users
+          returnValue.people.pageInfo = fetchMoreResult.data.people.pageInfo
+        }
+        return returnValue
+      }
+    })
+  }
+
+  handleNextPageClick = () => {
+    this.changePage(1, this.state.pageSize)
   }
 
   handlePreviousPageClick = () => {
-    const { limit, offset } = this.props.users.people.pageInfo
-    const currentPage = Math.floor(offset / limit)
-    const newPage = Math.max(0, currentPage - 1)
-    this.props.onPageChanged(newPage)
+    this.changePage(-1, this.state.pageSize)
   }
 
   handleRowSizeChanged = (index, value) => {
-    this.props.onPageSizeChanged(value)
+    this.changePage(1, value)
+    this.setState({ pageSize: value })
+  }
+
+  handleChange = async (userId, value) => {
+    await this
+      .props
+      .mutations
+      .editOrganizationRoles(this.props.organizationId, this.props.campaignsFilter.campaignId, userId, [value])
+  }
+
+  requestUserEditClose = () => {
+    this.setState({userEdit: false})
+  }
+
+  handleInviteTexterOpen() {
+    this.setState({ open: true })
+  }
+
+  handleInviteTexterClose() {
+    this.setState({ open: false})
+  }
+  handlePasswordResetClose() {
+    this.setState({ passwordResetHash: '' })
   }
 
   render() {
@@ -72,7 +237,17 @@ export class PeopleList extends Component {
       return <LoadingIndicator />
     }
 
+    if (!this.props.users.people.users.length) {
+      return (
+        <Empty
+          title='No people yet'
+          icon={<PeopleIcon />}
+        />
+      )
+    }
+
     const { users, pageInfo } = this.props.users.people
+    const { organizationId } = this.props
     const { limit, offset, total } = pageInfo
     const displayPage = Math.floor(offset / limit) + 1
     const tableData = prepareDataTableData(users)
@@ -82,26 +257,81 @@ export class PeopleList extends Component {
           data={tableData}
           columns={this.prepareTableColumns()}
           page={displayPage}
-          rowSize={limit}
+          rowSize={this.state.pageSize}
           count={total}
           onNextPageClick={this.handleNextPageClick}
           onPreviousPageClick={this.handlePreviousPageClick}
           onRowSizeChange={this.handleRowSizeChanged}
         />
+        {this.props.organizationId && ( 
+          <div>
+        <UserEditDialog
+          open={!!this.state.userEdit}
+          organizationId={organizationId}
+          userId={this.state.userEdit}
+          updateUser={this.updateUser}
+          requestClose={this.requestUserEditClose}
+        />
+        <ResetPasswordDialog
+          open={!!this.state.passwordResetHash}
+          requestClose={this.handlePasswordResetClose}
+          passwordResetHash={this.state.passwordResetHash}
+        />
+        </div>
+        )}
       </div>
     )
   }
 }
 
 PeopleList.propTypes = {
+  mutations: type.object,
+  users: type.object,
+  params: type.object,
   organizationId: type.string,
-  cursor: type.object,
   campaignsFilter: type.object,
-  onPageChanged: type.func,
-  onPageSizeChanged: type.func,
   utc: type.string,
-  users: type.object
+  currentUser: type.object
 }
+
+const organizationFragment = `
+  id
+  people(campaignId: $campaignId) {
+    id
+    displayName
+    email
+    roles(organizationId: $organizationId)
+  }
+`
+
+const mapMutationsToProps = ({ ownProps }) => ({
+  editOrganizationRoles: (organizationId, campaignId, userId, roles) => ({
+    mutation: gql`
+      mutation editOrganizationRoles($organizationId: String!, $userId: String!, $roles: [String], $campaignId: String) {
+        editOrganizationRoles(organizationId: $organizationId, userId: $userId, roles: $roles, campaignId: $campaignId) {
+          ${organizationFragment}
+        }
+      }
+    `,
+    variables: {
+      organizationId,
+      userId,
+      roles,
+      campaignId
+    }
+  }),
+  resetUserPassword: (organizationId, userId) => ({
+    mutation: gql`
+      mutation resetUserPassword($organizationId: String!, $userId: Int!) {
+        resetUserPassword(organizationId: $organizationId, userId: $userId)
+      }
+    `,
+    variables: {
+      organizationId,
+      userId
+    }
+  })
+})
 
 const mapQueriesToProps = ({ ownProps }) => ({
   users: {
@@ -135,14 +365,14 @@ const mapQueriesToProps = ({ ownProps }) => ({
         }
     `,
     variables: {
-      cursor: ownProps.cursor,
+      cursor: { offset: 0, limit: INITIAL_PAGE_SIZE },
       organizationId: ownProps.organizationId,
       campaignsFilter: ownProps.campaignsFilter,
-      sortBy: ownProps.sortBy || 'FIRST_NAME'
+      sortBy: ownProps.sortBy || 'FIRST_NAME',
     },
     forceFetch: true
   }
 })
 
 
-export default loadData(withRouter(PeopleList), { mapQueriesToProps })
+export default loadData(withRouter(PeopleList), { mapQueriesToProps, mapMutationsToProps })
--- src/containers/AdminPersonList.jsx
+++ src/containers/AdminPersonList.jsx
@@ -1,26 +1,17 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { withRouter } from 'react-router'
-import Empty from '../components/Empty'
 import OrganizationJoinLink from '../components/OrganizationJoinLink'
-import PasswordResetLink from '../components/PasswordResetLink'
-import UserEdit from './UserEdit'
 import FlatButton from 'material-ui/FlatButton'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import DropDownMenu from 'material-ui/DropDownMenu'
 import MenuItem from 'material-ui/MenuItem'
 import ContentAdd from 'material-ui/svg-icons/content/add'
-import { Table, TableBody, TableRow, TableRowColumn } from 'material-ui/Table'
 import Dialog from 'material-ui/Dialog'
-import PeopleIcon from 'material-ui/svg-icons/social/people'
-import { getHighestRole, ROLE_HIERARCHY } from '../lib'
 import theme from '../styles/theme'
 import loadData from './hoc/load-data'
 import gql from 'graphql-tag'
 import { dataTest } from '../lib/attributes'
-import LoadingIndicator from '../components/LoadingIndicator'
-import PaginatedUsersRetriever from './PaginatedUsersRetriever'
-
 import PeopleList from '../components/PeopleList'
 
 class AdminPersonList extends React.Component {
@@ -29,21 +20,12 @@ class AdminPersonList extends React.Component {
     super(props)
     this.handleOpen = this.handleOpen.bind(this)
     this.handleClose = this.handleClose.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.updateUser = this.updateUser.bind(this)
 
     this.state = {
       open: false,
       userEdit: false,
       passwordResetHash: '',
-      sortBy: this.FIRST_NAME_SORT.value,
-      people: [],
-      forceUpdateTime: Date.now(),
-
-      page: 0,
-      pageSize: 10,
-      needsRender: false,
-      utc: Date.now().toString(),
+      sortBy: this.FIRST_NAME_SORT.value
     }
   }
 
@@ -92,43 +74,8 @@ class AdminPersonList extends React.Component {
     this.setState({ open: false, passwordResetHash: '' })
   }
 
-  handleChange = async (userId, value) => {
-    await this
-      .props
-      .mutations
-      .editOrganizationRoles(this.props.params.organizationId, userId, [value])
-  }
-
   handleSortByChanged = (event, index, sortBy) => {
     this.setState({ sortBy })
-  }
-
-  handlePeopleReceived = (people) => {
-    this.setState({ people })
-  }
-
-  editUser(userId) {
-    this.setState({ 
-      userEdit: userId
-    })
-  }
-
-  updateUser() {
-    this.setState({ 
-      userEdit: false,
-      forceUpdateTime: Date.now()
-    })
-  }
-
-  async resetPassword(userId) {
-    const { userData: { currentUser } } = this.props
-    if (currentUser.id !== userId) {
-      const res = await this
-        .props
-        .mutations
-        .resetUserPassword(this.props.params.organizationId, userId)
-      this.setState({ passwordResetHash: res.data.resetUserPassword })
-    }
   }
 
   renderCampaignList = () => {
@@ -167,94 +114,20 @@ class AdminPersonList extends React.Component {
     </DropDownMenu>
   )
 
-  renderTexters() {
-    const { userData: { currentUser } } = this.props
-    if (!currentUser) return <LoadingIndicator />
-
-    if (this.state.people.length === 0) {
-      return (
-        <Empty
-          title='No people yet'
-          icon={<PeopleIcon />}
-        />
-      )
-    }
-
-    return (
-      <Table selectable={false}>
-        <TableBody
-          displayRowCheckbox={false}
-          showRowHover
-        >
-          {this.state.people.map((person) => (
-            <TableRow
-              key={person.id}
-            >
-              <TableRowColumn>{person.displayName}</TableRowColumn>
-              <TableRowColumn>{person.email}</TableRowColumn>
-              <TableRowColumn>
-                <DropDownMenu
-                  value={getHighestRole(person.roles)}
-                  disabled={person.id === currentUser.id || getHighestRole(person.roles) === 'OWNER' && getHighestRole(currentUser.roles) !== 'OWNER'}
-                  onChange={(event, index, value) => this.handleChange(person.id, value)}
-                >
-                  {ROLE_HIERARCHY.map((option) => (
-                    <MenuItem
-                      key={person.id + '_' + option}
-                      value={option}
-                      disabled={option === 'OWNER' && getHighestRole(currentUser.roles) !== 'OWNER'}
-                      primaryText={`${option.charAt(0).toUpperCase()}${option.substring(1).toLowerCase()}`}
-                    />
-                  ))}
-                </DropDownMenu>
-                <FlatButton
-                  {...dataTest('editPerson')}
-                  label='Edit'
-                  onTouchTap={() => { this.editUser(person.id) }}
-                />
-                <FlatButton
-                  label='Reset Password'
-                  disabled={currentUser.id === person.id}
-                  onTouchTap={() => { this.resetPassword(person.id) }}
-                />
-              </TableRowColumn>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    )
-  }
-  handlePageChange = async (page) => {
-    await this.setState({
-      page,
-      needsRender: true
-    })
-  }
-
-  handlePageSizeChange = async (pageSize) => {
-    await this.setState({ needsRender: true, pageSize })
-  }
-
   render() {
     const { organizationData } = this.props
-
-    const cursor = {
-      offset: this.state.page * this.state.pageSize,
-      limit: this.state.pageSize
-    }
+    const { userData: { currentUser } } = this.props
 
     return (
       <div>
-        <PeopleList
-          organizationId={this.props.params.organizationId}
-          cursor={cursor}
-          campaignsFilter={{ campaignId: this.props.location.query.campaignId && parseInt(this.props.location.query.campaignId, 10) }}
-          utc={this.state.utc}
-          onPageChanged={this.handlePageChange}
-          onPageSizeChanged={this.handlePageSizeChange}
-        />
         {this.renderCampaignList()}
         {this.renderSortBy()}
+        <PeopleList
+          organizationId={organizationData.organization && organizationData.organization.id}
+          campaignsFilter={{ campaignId: this.props.location.query.campaignId && parseInt(this.props.location.query.campaignId, 10) }}
+          utc={this.state.utc}
+          currentUser={currentUser}
+        />
         <FloatingActionButton
           {...dataTest('addPerson')}
           style={theme.components.floatingButton}
@@ -264,19 +137,6 @@ class AdminPersonList extends React.Component {
         </FloatingActionButton>
         {organizationData.organization && (
           <div>
-            <Dialog
-              {...dataTest('editPersonDialog')}
-              title='Edit user'
-              modal={false}
-              open={Boolean(this.state.userEdit)}
-              onRequestClose={() => { this.setState({ userEdit: false }) }}
-            >
-              <UserEdit
-                organizationId={organizationData.organization && organizationData.organization.id}
-                userId={this.state.userEdit}
-                onRequestClose={this.updateUser}
-              />
-            </Dialog>
             <Dialog
               title='Invite new texters'
               actions={[
@@ -295,24 +155,6 @@ class AdminPersonList extends React.Component {
                 organizationUuid={organizationData.organization.uuid}
               />
             </Dialog>
-            <Dialog
-              title='Reset user password'
-              actions={[
-                <FlatButton
-                  {...dataTest('passResetOK')}
-                  label='OK'
-                  primary
-                  onTouchTap={this.handleClose}
-                />
-              ]}
-              modal={false}
-              open={Boolean(this.state.passwordResetHash)}
-              onRequestClose={this.handleClose}
-            >
-              <PasswordResetLink
-                passwordResetHash={this.state.passwordResetHash}
-              />
-            </Dialog>
           </div>
         )}
       </div>
@@ -328,44 +170,6 @@ AdminPersonList.propTypes = {
   router: PropTypes.object,
   location: PropTypes.object
 }
-
-const organizationFragment = `
-  id
-  people(campaignId: $campaignId) {
-    id
-    displayName
-    email
-    roles(organizationId: $organizationId)
-  }
-`
-const mapMutationsToProps = ({ ownProps }) => ({
-  editOrganizationRoles: (organizationId, userId, roles) => ({
-    mutation: gql`
-      mutation editOrganizationRoles($organizationId: String!, $userId: String!, $roles: [String], $campaignId: String) {
-        editOrganizationRoles(organizationId: $organizationId, userId: $userId, roles: $roles, campaignId: $campaignId) {
-          ${organizationFragment}
-        }
-      }
-    `,
-    variables: {
-      organizationId,
-      userId,
-      roles,
-      campaignId: ownProps.location.query.campaignId
-    }
-  }),
-  resetUserPassword: (organizationId, userId) => ({
-    mutation: gql`
-      mutation resetUserPassword($organizationId: String!, $userId: Int!) {
-        resetUserPassword(organizationId: $organizationId, userId: $userId)
-      }
-    `,
-    variables: {
-      organizationId,
-      userId
-    }
-  })
-})
 
 const mapQueriesToProps = ({ ownProps }) => ({
   userData: {
@@ -402,4 +206,4 @@ const mapQueriesToProps = ({ ownProps }) => ({
   }
 })
 
-export default loadData(withRouter(AdminPersonList), { mapQueriesToProps, mapMutationsToProps })
+export default loadData(withRouter(AdminPersonList), { mapQueriesToProps })
