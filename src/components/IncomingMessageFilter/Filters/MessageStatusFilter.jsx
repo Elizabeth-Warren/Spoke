a5a9b7578fa9e6c017e4f55a2bf981e24be5ee63
Larry Person <lp10011@gmail.com>
2019-11-03 00:49:22 -0400

    add clear button for which messages

--- src/components/IncomingMessageFilter/Filters/MessageStatusFilter.jsx
+++ src/components/IncomingMessageFilter/Filters/MessageStatusFilter.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import type from "prop-types";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
+import IconButton from "material-ui/IconButton";
+import ClearIcon from "material-ui/svg-icons/content/clear";
+import { StyleSheet, css } from "aphrodite";
+
+const styles = StyleSheet.create({
+  commas: {
+    ":not(:last-child)::after": {
+      content: '", "'
+    }
+  },
+  icon: {}
+});
 
 export const MESSAGE_STATUSES = {
   all: {
@@ -38,6 +50,26 @@ export const MessageStatusFilter = props => (
     floatingLabelText={"Contact message status"}
     floatingLabelFixed
     onChange={props.onChange}
+    selectionRenderer={(value, menuItems) =>
+      menuItems.length ? (
+        <div>
+          <IconButton
+            className={css(styles.icon)}
+            onClick={event => {
+              event.stopPropagation();
+              props.onChange([]);
+            }}
+          >
+            <ClearIcon />
+          </IconButton>
+          {menuItems.map(menuItem => (
+            <span className={css(styles.commas)}>
+              {menuItem.props.primaryText}
+            </span>
+          ))}
+        </div>
+      ) : null
+    }
   >
     {Object.keys(MESSAGE_STATUSES).map(messageStatus => {
       const displayText = MESSAGE_STATUSES[messageStatus].name;
