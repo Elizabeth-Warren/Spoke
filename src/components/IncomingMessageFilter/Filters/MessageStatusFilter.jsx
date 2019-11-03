b9d6b9301620f034c1052e65487b2e60d209006d
Larry Person <lp10011@gmail.com>
2019-11-03 00:00:25 -0400

    ran prettier

--- src/components/IncomingMessageFilter/Filters/MessageStatusFilter.jsx
+++ src/components/IncomingMessageFilter/Filters/MessageStatusFilter.jsx
@@ -1,50 +1,48 @@
-
-import React from 'react'
-import type from 'prop-types'
-import SelectField from 'material-ui/SelectField'
-import MenuItem from 'material-ui/MenuItem'
+import React from "react";
+import type from "prop-types";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
 
 export const MESSAGE_STATUSES = {
   all: {
-    name: 'All',
-    children: ['needsResponse', 'needsMessage', 'convo', 'messaged']
+    name: "All",
+    children: ["needsResponse", "needsMessage", "convo", "messaged"]
   },
   needsResponse: {
-    name: 'Needs Texter Response',
+    name: "Needs Texter Response",
     children: []
   },
   needsMessage: {
-    name: 'Needs First Message',
+    name: "Needs First Message",
     children: []
   },
   convo: {
-    name: 'Active Conversation',
+    name: "Active Conversation",
     children: []
   },
   messaged: {
-    name: 'First Message Sent',
+    name: "First Message Sent",
     children: []
   },
   closed: {
-    name: 'Closed',
+    name: "Closed",
     children: []
   }
-}
+};
 
-export const MessageStatusFilter = (props) =>
+export const MessageStatusFilter = props => (
   <SelectField
     multiple
     value={props.messageFilter}
-    hintText={'Which messages?'}
-    floatingLabelText={'Contact message status'}
+    hintText={"Which messages?"}
+    floatingLabelText={"Contact message status"}
     floatingLabelFixed
     onChange={props.onChange}
   >
     {Object.keys(MESSAGE_STATUSES).map(messageStatus => {
-      const displayText = MESSAGE_STATUSES[messageStatus].name
+      const displayText = MESSAGE_STATUSES[messageStatus].name;
       const isChecked =
-        props.messageFilter &&
-        props.messageFilter.indexOf(messageStatus) > -1
+        props.messageFilter && props.messageFilter.indexOf(messageStatus) > -1;
       return (
         <MenuItem
           key={messageStatus}
@@ -53,14 +51,14 @@ export const MessageStatusFilter = (props) =>
           insetChildren
           checked={isChecked}
         />
-      )
+      );
     })}
   </SelectField>
+);
 
 MessageStatusFilter.propTypes = {
   messageFilter: type.array.isRequired,
   onChange: type.func.isRequired
-}
+};
 
-
-export default MessageStatusFilter
+export default MessageStatusFilter;
