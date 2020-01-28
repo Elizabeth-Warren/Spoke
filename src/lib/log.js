b97079a63c60f67bc29f1ea4fcc6f7cb31796455
Matteo Banerjee <mbanerjee@elizabethwarren.com>
2020-01-28 10:44:43 -0500

    Remove the rollbar and the fs packages (#49)
    The "fs" package is just a placeholder for what seems to have once
    been a malicious [package](https://www.npmjs.com/package/fs).
    
    The reason it was in there in the first place was to resolve a webpack
    issue with the Rollbar sdk, which is intended to work on both the
    client and server.
    
    Since we don't use Rollbar, I decided to just remove this package.
    If we did use it, it would be enough to add the following block
    to the webpack config:
    
    ```
    node: {
      fs: "empty"
    }
    ```
    
    Which seems to be on of many ways to handle this issue.

--- src/lib/log.js
+++ src/lib/log.js
@@ -1,29 +1,12 @@
 import minilog from "minilog";
 import { isClient } from "./is-client";
-const rollbar = require("rollbar");
-let logInstance = null;
+
+let logInstance;
 
 if (isClient()) {
   minilog.enable();
   logInstance = minilog("client");
-  const existingErrorLogger = logInstance.error;
-  logInstance.error = (...err) => {
-    const errObj = err;
-    if (window.Rollbar) {
-      window.Rollbar.error(...errObj);
-    }
-    existingErrorLogger.call(...errObj);
-  };
 } else {
-  let enableRollbar = false;
-  if (
-    process.env.NODE_ENV === "production" &&
-    process.env.ROLLBAR_ACCESS_TOKEN
-  ) {
-    enableRollbar = true;
-    rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
-  }
-
   minilog.suggest.deny(
     /.*/,
     process.env.NODE_ENV === "development" ? "debug" : "debug"
@@ -37,16 +20,6 @@ if (isClient()) {
   logInstance = minilog("backend");
   const existingErrorLogger = logInstance.error;
   logInstance.error = err => {
-    if (enableRollbar) {
-      if (typeof err === "object") {
-        rollbar.handleError(err);
-      } else if (typeof err === "string") {
-        rollbar.reportMessage(err);
-      } else {
-        rollbar.reportMessage("Got backend error with no error message");
-      }
-    }
-
     existingErrorLogger(err && err.stack ? err.stack : err);
   };
 }
