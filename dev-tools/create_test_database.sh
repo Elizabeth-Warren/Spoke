8a4ba1b1309148d25d920ab0b308d39813bdc0ca
Matteo Banerjee <mbanerjee@elizabethwarren.com>
2020-01-29 21:59:48 -0500

    Fix create_test_database and move it to dev-tools (#70)

--- dev-tools/create_test_database.sh
+++ dev-tools/create_test_database.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 docker exec -i internal-spoke_postgres_1  psql  -h localhost -p 5432 -U spoke spokedev <<EOF
        CREATE DATABASE spoke_test;
-       CREATE USER DATABASE spoke_test WITH PASSWORD 'spoke_test';
+       CREATE USER spoke_test WITH PASSWORD 'spoke_test';
        GRANT ALL PRIVILEGES ON DATABASE spoke_test TO spoke_test;
 EOF
