a21dc4eb20304679964f5871edd4971fb87b5754
Matteo Banerjee <mbanerjee@elizabethwarren.com>
2020-01-31 20:22:12 -0500

    Teach buy-numbers to add to messaging services and deal with twilio limits (#98)

--- dev-tools/buy-numbers.js
+++ dev-tools/buy-numbers.js
@@ -3,57 +3,81 @@ const parseArgs = require("minimist");
 const twilio = require("twilio");
 require("dotenv").config();
 
-async function main() {
-  let successCount = 0;
-  try {
-    const client = twilio(
-      process.env.TWILIO_ACCOUNT_SID,
-      process.env.TWILIO_AUTH_TOKEN
-    );
+// maximum when listing available numbers
+const MAX_PAGE_SIZE = 30;
 
-    // TODO: validation
-    const {
+const {
+  areaCode,
+  limit,
+  friendlyNamePrefix,
+  voiceUrl,
+  smsUrl,
+  messagingServiceSid,
+  // TODO: add an option to request specific capabilities
+  addToSpokeInventory // TODO: implement this once Spoke tables are created
+} = parseArgs(process.argv, { default: { limit: 1 } });
+
+// TODO: more validation
+if (typeof areaCode !== "number" || areaCode > 999) {
+  console.error(`Invalid area code: ${areaCode}`);
+  process.exit();
+}
+
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+let successCount = 0;
+
+async function buy(max) {
+  console.log("Attempting to buy: ", max);
+  const nums = await client
+    .availablePhoneNumbers("US")
+    .local.list({
       areaCode,
-      limit,
-      friendlyNamePrefix,
-      voiceUrl,
-      smsUrl,
-      addToSpokeInventory // TODO: implement this once Spoke tables are created
-    } = parseArgs(process.argv, { default: { limit: 1 } });
+      limit: max,
+      pageSize: MAX_PAGE_SIZE,
+      capabilities: ["SMS"]
+    });
 
-    if (typeof areaCode !== "number" || areaCode > 999) {
-      console.error(`Invalid area code: ${areaCode}`);
-      return;
+  for (const num of nums) {
+    console.log(`Buying Number: ${num.phoneNumber}`);
+    const opts = { phoneNumber: num.phoneNumber, voiceUrl, smsUrl };
+    if (friendlyNamePrefix) {
+      opts.friendlyName = `${friendlyNamePrefix} ${num.phoneNumber}`;
     }
-
-    // Note: will only fetch one page of results, but you don't actually want to
-    // buy more than a 1000 numbers at a time, right?
-    const nums = await client
-      .availablePhoneNumbers("US")
-      .local
-      .list({
-        areaCode,
-        limit,
-        pageSize: 1000,
-        capabilities: ["SMS", "voice"]
-      });
-
-    if (nums.length === 0) {
-      console.log("Didn't find any numbers");
-      return;
+    const result = await client.incomingPhoneNumbers.create(opts);
+    if (messagingServiceSid) {
+      console.log(
+        `Associating ${result.sid} with service ${messagingServiceSid}`
+      );
+      await client.messaging
+        .services(messagingServiceSid)
+        .phoneNumbers.create({ phoneNumberSid: result.sid });
     }
+    successCount++;
+  }
+  return nums.length;
+}
 
-    for (const num of nums) {
-      console.log(`Buying Number: ${num.phoneNumber}`);
-      const opts = { phoneNumber: num.phoneNumber, voiceUrl, smsUrl };
-      if (friendlyNamePrefix) {
-        opts.friendlyName = `${friendlyNamePrefix} ${num.phoneNumber}`;
+async function main() {
+  try {
+    while (successCount < limit) {
+      const nextBatch = Math.min(MAX_PAGE_SIZE, limit - successCount);
+      const found = await buy(nextBatch);
+      if (found === 0) {
+        console.log("Didn't find any numbers :(");
+        break;
       }
-      await client.incomingPhoneNumbers.create(opts);
-      successCount++;
     }
   } catch (e) {
     console.error(e);
+  }
+  if (messagingServiceSid) {
+    console.log(
+      `Link to your messaging service: https://www.twilio.com/console/sms/services/${messagingServiceSid}/numbers`
+    );
   }
   // Log the number we bought successfully if we fail part way through
   console.log(`Bought ${successCount} number(s)`);
