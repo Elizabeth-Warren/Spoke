97292b0f1373b3b8c904b633c8c35f3598298c69
Julia Schultz <jschultz@elizabethwarren.com>
2020-02-06 09:42:02 -0500

    add onClick to save button (#169)
    * add onClick to save button
    
    * simplify

--- src/components/CampaignCannedResponsesForm.jsx
+++ src/components/CampaignCannedResponsesForm.jsx
@@ -163,6 +163,9 @@ export default class CampaignCannedResponsesForm extends React.Component {
           type="submit"
           disabled={this.props.saveDisabled}
           label={this.props.saveLabel}
+          onClick={() => {
+            !this.props.saveDisabled && this.props.onSubmit();
+          }}
         />
       </GSForm>
     );
