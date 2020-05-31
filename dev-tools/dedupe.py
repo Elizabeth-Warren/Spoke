21e62f71a6d276e4b936ff16a50b6b7f8d6cca9b
John Talbott <john@paywithpeach.com>
2020-05-31 16:38:12 +0100

    Verify header in deduper

--- dev-tools/dedupe.py
+++ dev-tools/dedupe.py
@@ -4,28 +4,51 @@ import csv
 import re
 import sys, getopt
 
-# Open details file and get a unique set of links
-phone_numbers_set = set();
-phone_number_regex = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+def verifyFieldnames(fieldnames):
+  fieldnames_set = set(fieldnames)
+  for fieldname in fieldnames:
+    fieldnames_set.add(fieldname.strip())
+  
+  expected_fieldnames = {'Voter File VANID', 'LastName', 'FirstName', 'Cell Phone'}
+  return expected_fieldnames.issubset(fieldnames_set)
 
+  expected_fieldnames_set = {'LastName', 'FirstName', 'Cell Phone', }
+  
 def processFile(INFILE, OUTFILE, TESTMODE=False):
+  phone_numbers_set = set()
+  phone_numbers_dupes = []
+  infile_length = 0
+  outfile_length = 0
+  last_names_with_invalid_numbers = []
+  phone_number_regex = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+
   with open(OUTFILE, 'w') as dedupe_csvfile:
     fieldnames = ['first_name', 'last_name', 'phone_number', 'source_id_type', 'source_id']
     writer = csv.DictWriter(dedupe_csvfile, fieldnames=fieldnames)
 
     if (TESTMODE != True):
       writer.writeheader()
-    
+    else:
+      print "IN TESTMODE: not writing any output"
+
     with open(INFILE, 'rb') as csvfile:
       csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='\'')
+      if verifyFieldnames(csvreader.fieldnames):
+        print 'Verified fieldnames'
+      else:
+        print 'Aborting, could not Verify fieldnames in file:', INFILE
+        sys.exit(1) 
 
       for row in csvreader:
+        infile_length += 1
         phone_number = row.get('Cell Phone', row.get('Cell Phone ', ''));
+        last_name = row.get('LastName', row.get('LastName ', ''));
 
         if bool(phone_number_regex.match(phone_number)):
           if phone_number in phone_numbers_set:
-            print '{} is a duplicate'.format(phone_number);
+            phone_numbers_dupes.append(phone_number)
           else:
+            phone_numbers_set.add(phone_number);
             if (TESTMODE != True):
               writer.writerow({
                   'first_name': row['FirstName'], 
@@ -33,10 +56,22 @@ def processFile(INFILE, OUTFILE, TESTMODE=False):
                   'phone_number': phone_number,
                   'source_id_type': 'VAN',
                   'source_id': row['Voter File VANID']})
-              phone_numbers_set.add(phone_number);
+              outfile_length += 1
+            else:
+              print "IN TESTMODE: not writing any output"
         else:
-          print '{} does not look like a real phone number on row: \n\t{}'.format(phone_number, row);
+          last_names_with_invalid_numbers.append(last_name)
+  
+  print '--------------------------------------'
+  print 'Processed:', INFILE
+  print 'INFILE length:', infile_length
+  print 'Dupes found:', len(phone_numbers_dupes)
+  print 'Lines skipped because of a bad number:', len(last_names_with_invalid_numbers)
+  print 'Created (or not if in test mode):', OUTFILE
+  print 'OUTFILE length:', outfile_length
+  print 'Test mode is:', TESTMODE
 
+  
 def main(argv):
   INFILE = ''
   OUTFILE = ''
@@ -57,10 +92,6 @@ def main(argv):
       OUTFILE = arg
     elif opt in ("-t", "--testmode"):
       TESTMODE = True
-
-  print 'Input file is:', INFILE
-  print 'Output file is:', OUTFILE
-  print 'Test mode is:', TESTMODE
 
   processFile(INFILE, OUTFILE, TESTMODE)
 
